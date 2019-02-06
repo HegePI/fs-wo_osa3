cc9058045df4735cf696968087a24453edb19f7b
Heikki <19744083+HegePI@users.noreply.github.com>
2019-02-06 19:46:02 +0200

    osa 3 tehtävät 3.1 - 3.6

---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "045-1236543"
+    },
+    {
+        id: 2,
+        name: "Arto Järvinen",
+        number: "041-21423123"
+    },
+    {
+        id: 3,
+        name: "Lea Kutvonen",
+        number: "0404323234"
+    },
+    {
+        id: 4,
+        name: "Martti Tienari",
+        number: "09-784232"
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+
+
+})
+
+app.get('/info', (reg, res) => {
+    const date = new Date()
+    res.send(`
+    <div>
+    <p>Puhelinluettelossa ${persons.length} henkilön tiedot
+    </p>
+    <p>${date}</p>
+    </div>`)
+
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter(person => person.id !== id);
+
+    response.status(204).end();
+})
+
+const generateID = () => {
+    return Math.floor(Math.random() * Math.floor(1000000));
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(body)
+
+    if (body.name === undefined) {
+        return response.status(400).json({
+            error: 'name is missing'
+        })
+    }
+
+    if (body.number === undefined) {
+        return response.status(400).json({
+            error: `number is missing`
+        })
+    }
+
+    if (persons.some(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: `name must be unique`
+        })
+    }
+
+
+
+    const newPerson = {
+        id: generateID(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(newPerson)
+
+    response.json(newPerson)
+});
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
