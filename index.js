6dc0ccff91a8e8f8cfed57144a4ac30063c740ae
Heikki <19744083+HegePI@users.noreply.github.com>
2019-02-11 23:08:54 +0200

    osa3 teht. 3.1 - 3.16

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
-const person = require('./models/person')
+const Person = require('./models/person')
 
 
 
@@ -13,30 +14,52 @@ morgan.token('body', function body(req) {
 
 })
 
+const logger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(morgan(':method :url :response-time :body'))
 app.use(cors())
-app.use(express.static('build'))
+app.use(logger)
+
 
 app.get('/api/persons', (request, response) => {
     console.log('Haetaan henkilöitä tietokannasta')
-    person.find({}).then(person => {
+    Person.find({}).then(person => {
         response.json(person.map(person => person.toJSON()))
     })
+        .catch(error => {
+            console.log(error);
+            response.status(404).end()
+        })
 
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    person.findById(request.params.id).then(person => {
+    Person.findById(request.params.id).then(person => {
         response.json(person.toJSON())
     })
+        .catch(error => {
+            console.log(error);
+            response.status(404).end()
+        })
 })
 
 app.get('/info', (reg, res) => {
+    console.log('moi')
     const date = new Date()
+    const amount = Person.distinct('_id').length
+    console.log(amount)
+
     res.send(`
     <div>
-    <p>Puhelinluettelossa ${persons.length} henkilön tiedot
+    <p>Puhelinluettelossa ${amount} henkilön tiedot
     </p>
     <p>${date}</p>
     </div>`)
@@ -44,22 +67,16 @@ app.get('/info', (reg, res) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(person => person.id !== id);
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 
-    response.status(204).end();
 })
 
-const generateID = () => {
-    return Math.floor(Math.random() * Math.floor(1000000));
-}
-
 app.post('/api/persons', (request, response) => {
-    console.log('Moi backendista')
-
     const body = request.body
-
-    console.log(body)
 
     if (body.name === undefined) {
         return response.status(400).json({
@@ -73,21 +90,10 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    //if (persons.some(person => person.name === body.name)) {
-    //  return response.status(400).json({
-    //    error: `name must be unique`
-    //})
-    //}
-
-
-    console.log('Hetki ennen luomista')
-
-    const newPerson = new person({
-        //id: generateID(),
+    const newPerson = new Person({
         name: body.name,
         nro: body.nro
     })
-    console.log('Luominen onnistui')
 
     morgan.compile(JSON.stringify(body))
 
@@ -95,10 +101,43 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson.toJSON())
     })
         .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'malformatted id' })
+            console.log(error);
+            response.status(404).end()
         })
 });
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        nro: body.nro,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson.toJSON())
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
